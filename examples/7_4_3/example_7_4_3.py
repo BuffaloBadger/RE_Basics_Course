92e43c9ab9d004a57697af8ca3eb9e4f8873abb8
Carl Lund <buffalobadger@gmail.com>
2026-05-12 10:55:24 -0400

    added and re-ordered examples

--- examples/7_4_3/example_7_4_3.py
+++ examples/7_4_3/example_7_4_3.py
@@ -2,189 +2,240 @@
 
 # import libraries
 import numpy as np
-import pandas as pd
-import scipy as sp
-import matplotlib.pyplot as plt
 from reb_utils import solve_ivodes
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# set resolution for the graph
+# set plot resolution
 plt.rc('savefig', dpi=300)
 
-# global constants available to all functions
+# global constants available in all functions
 # given
-PA_0 = 1. # atm
-PB_0 = 2. # atm
-T_0 = 25 + 273.14 # K
-V = 2. # L
-Aex = 600. # cm^2
-Uex = 0.6 # cal /cm^2 /min /K
-Tex = 30 + 273.15 # K
-k_0_1 =  3.34E9 # mol /cm^3 /min /atm^2
-k_0_2 = 4.99E9 # mol /cm^3 /min /atm^2
-E_1 = 20.5E3 # cal /mol
-E_2 = 21.8E3 # cal /mol
-dH_1 = -6300. # cal /mol
-dH_2 = -6900. # cal /mol
-Cp_A = 7.4 # cal /mol /K
-Cp_B = 8.6 # cal /mol /K
-Cp_D = 10.7 # cal /mol /K
-Cp_Z = 5.2 # cal /mol /K
-Cp_U = 10.3 # cal /mol /K
+k0_1 = 2.59E9 # /min
+E_1 = 16500. # cal /mol
+dH_1 = -22200. # cal /mol
+CA_0 = 2. # mol /L
+T_0 = 23  + 273.15 # K
+Cp = 440. # cal /L /K
+V = 4.0 # L
+V_shell = 0.5 # L
+A_shell = 0.6 # ft^2
+U_shell = 1.13E4/60 # cal /ft^2 /min /K
+Tex_in = 20 + 273.15 # K
+rho_ex = 1.0 # g /cm^3
+Cp_ex = 1.0 # cal /g /K
+U_coil = 3.8E4/60 # cal /ft^2 /min /K
+A_coil = 0.23 # ft^2
+T_coil = 120 + 273.15 # K
+Tex_0 = 23 + 273.15 # K
+T_1 = 50 + 273.15 # K
+T_f = 25 + 273.15 # K
+t_turn = 25 # min
 # known
-Re = 1.987 # cal /mol /K
-Rw = 82.057 # cm^3 atm /mol /K
+R = 1.987 # cal /mol /K
 # calculated
-nA_0 = PA_0*V/Rw/T_0
-nB_0 = PB_0*V/Rw/T_0
-P_0 = PA_0 + PB_0
+nA_0 = CA_0*V
 
-# BSTR model function
-def bstr_model_variables(t_f):
+# allocate global storage for the current coolant flow rate
+g_Vdot_ex = float('nan')
+
+# BSTR reactor model for the first stage of the protocal
+def stage1_bstr_model_variables():
 	# set the initial values
     ind_0 = 0.0
-    dep_0 = np.array([nA_0, nB_0, 0.0, 0.0, 0.0, T_0, P_0])
+    dep_0 = np.array([nA_0, 0.0, T_0, Tex_0])
 
 	# define the stopping criterion
-    f_var = 0
-    f_val = t_f
+    f_var = 3
+    f_val = T_1
+    
+	# solve the IVODEs
+    t, dep, success, message = solve_ivodes(ind_0, dep_0, f_var, f_val
+            , stage1_bstr_derivatives, odes_are_stiff=False)
+    
+    # check for solver issues
+    if not(success):
+        print('')
+        print(f"Stage 1 BSTR model issue: {message}")
+        print('')
+        input('Press return to continue.')
+
+    # extract the dependent variable profiles
+    nA = dep[0,:]
+    nZ = dep[1,:]
+    T = dep[2,:]
+    Tex = dep[3,:]
+
+    # return the first stage model variables
+    return t, nA, nZ, T, Tex
+
+# derivatives function for the first stage of operation
+def stage1_bstr_derivatives(ind, dep):
+	# extract the dependent variables
+    nA = dep[0]
+    T = dep[2]
+    Tex = dep[3]
+
+	# calculate the rate
+    CA = nA/V
+    k_1 = k0_1*np.exp(-E_1/R/T)
+    r_1 = k_1*CA
+    
+    # calculate the rates of heat exchange
+    Qdot_shell = U_shell*A_shell*(Tex - T)
+    Qdot_coil = U_coil*A_coil*(T_coil - T)
+
+	# evaluate the derivatives
+    dnAdt = -V*r_1
+    dnZdt = V*r_1
+    dTdt = (Qdot_shell + Qdot_coil - V*r_1*dH_1)/V/Cp
+    dTexdt = -Qdot_shell/rho_ex/V_shell/Cp_ex
+    
+	# return the derivatives
+    return [dnAdt, dnZdt, dTdt, dTexdt]
+
+# reactor model for the second stage of operation
+def stage2_bstr_model_variables(t_0, nA_0, nZ_0, T_0, Tex_0):
+	# set the initial values
+    ind_0 = t_0
+    dep_0 = np.array([nA_0, nZ_0, T_0, Tex_0])
+
+	# define the stopping criterion
+    f_var = 3
+    f_val = T_f
      
 	# solve the IVODEs
     t, dep, success, message = solve_ivodes(ind_0, dep_0, f_var, f_val
-                    , bstr_derivatives, odes_are_stiff=True)
+                , stage2_bstr_derivatives, odes_are_stiff=False)
 
     # check for solver issues
     if not(success):
         print('')
-        print(f"BSTR model function issue: {message}")
+        print(f"Stage 2 BSTR model issue: {message}")
         print('')
-        input('Press return to continue')
+        input('Press return to continue.')
 
     # extract the dependent variable profiles
     nA = dep[0,:]
-    nB = dep[1,:]
-    nD = dep[2,:]
-    nZ = dep[3,:]
-    nU = dep[4,:]
-    T = dep[5,:]
-    P = dep[6,:]
+    nZ = dep[1,:]
+    T = dep[2,:]
+    Tex = dep[3,:]
 
-    # return the reactor model variables
-    return t, nA, nB, nD, nZ, nU, T, P
+    # return the second stage model variables
+    return t, nA, nZ, T, Tex
 
-# derivatives function
-def bstr_derivatives(ind, dep):
+# derivatives function for the second stage of operation
+def stage2_bstr_derivatives(ind, dep):
 	# extract the dependent variables
     nA = dep[0]
-    nB = dep[1]
-    nD = dep[2]
-    nZ = dep[3]
-    nU = dep[4]
-    T = dep[5]
+    T = dep[2]
+    Tex = dep[3]
 
 	# calculate the rate
-    PA = nA*Rw*T/V
-    PB = nB*Rw*T/V
-    PD = nD*Rw*T/V
-    k_1 = k_0_1*np.exp(-E_1/Re/T)
-    k_2 = k_0_2*np.exp(-E_2/Re/T)
-    r_1 = k_1*PA*PB
-    r_2 = k_2*PD*PB
+    CA = nA/V
+    k_1 = k0_1*np.exp(-E_1/R/T)
+    r_1 = k_1*CA
 
-    # calculate the rate of heat exchange
-    Qdot = Uex*Aex*(Tex - T)
+    # calculate the rates of heat exchange
+    Qdot_shell = U_shell*A_shell*(Tex - T)
 
-	# Create mass matrix, setting all elements to zero
-    mass_matrix = np.zeros((7,7))
-
-    # Add 1 on the diagonal for the first 5 rows
-    mass_matrix[0,0] = 1.0
-    mass_matrix[1,1] = 1.0
-    mass_matrix[2,2] = 1.0
-    mass_matrix[3,3] = 1.0
-    mass_matrix[4,4] = 1.0
-
-    # Add the elements for the energy balance
-    mass_matrix[5,5] = nA*Cp_A + nB*Cp_B + nD*Cp_D + nZ*Cp_Z + nU*Cp_U
-    mass_matrix[5,6] = -V*Re/Rw
-
-    # Add the elements for the ideal gas law equation
-    mass_matrix[6,0] = Rw*T
-    mass_matrix[6,1] = Rw*T
-    mass_matrix[6,2] = Rw*T
-    mass_matrix[6,3] = Rw*T
-    mass_matrix[6,4] = Rw*T
-    mass_matrix[6,5] = Rw*(nA + nB + nD + nZ +nU)
-    mass_matrix[6,6] = -V
-
-    # Create right side vector
-    rhs1 = -r_1*V
-    rhs2 = (-r_1 -r_2)*V
-    rhs3 = (r_1-r_2)*V
-    rhs4 = (r_1+r_1)*V
-    rhs5 = r_2*V
-    rhs6 = Qdot -(r_1*dH_1 + r_2*dH_2)*V
-    rhs7 = 0.0
-    rhs = np.array([rhs1, rhs2, rhs3, rhs4, rhs5, rhs6, rhs7])
-
-    # Evaluate the derivatives
-    derivs = sp.linalg.solve(mass_matrix, rhs)
-
-    # Return the derivatives
-    return derivs
+	# evaluate the derivatives
+    dnAdt = -V*r_1
+    dnZdt = V*r_1
+    dTdt = (Qdot_shell - V*r_1*dH_1)/V/Cp
+    dTexdt = -(Qdot_shell + g_Vdot_ex*rho_ex*Cp_ex*(Tex-Tex_in))/rho_ex/V_shell/Cp_ex
+    
+	# return the derivatives
+    return [dnAdt, dnZdt, dTdt, dTexdt]
 
 # deliverables function
 def deliverables():
-    # choose a large final time
-    tf = 60 # min
+    # solve the reactor design equations for the first stage
+    t_1, nA_1, nZ_1, T_1, Tex_1 = stage1_bstr_model_variables()
 
-    # solve the BSTR design equations
-    t, nA, nB, nD, nZ, nU, T, P = bstr_model_variables(tf)
+    # allow this function to set g_Vdot_ex
+    global g_Vdot_ex
 
-    # calculate the corresponding yield
-    yield_DA = nD/nA_0
+    # choose a range of coolant flow rates
+    #coolant_flows = np.linspace(100.0, 500.0, 100)
+    coolant_flows = np.linspace(175.0, 200.0, 100)
 
-    # find the reaction time where the yield is maximized
-    i_opt = np.argmax(yield_DA)
+    # allocate storage for the corresponding net rates
+    net_rates = np.zeros(100)
 
-    # find the corresponding reaction time
-    t_opt = t[i_opt]
+    # calculate the net rate for each coolant flow rate
+    for i in range(0,100):
+        # make the coolant flow rate globally available
+        g_Vdot_ex = coolant_flows[i]
 
-    # check that the optimum time is between 0 and t_f
-    if (t_opt == 0) or (t_opt == tf):
-        print('')
-        print('The range of reaction times needs to be wider')
-        print('')
-        input('Press return to continue')
+        # solve the reactor design equations
+        t, nA, nZ, T, T_ex = stage2_bstr_model_variables(t_1[-1], nA_1[-1], nZ_1[-1]
+                                                , T_1[-1], Tex_1[-1])
+        
+        # calculate the net rate
+        net_rates[i] = nZ[-1]/(t[-1] + t_turn)
 
-    # calculate the optimum yield and conversion
-    yield_opt = yield_DA[i_opt]
-    f_opt = 100*(nA_0 - nA[i_opt])/nA_0
+    # find the coolant flow where the net rate is maximized
+    i_max = np.argmax(net_rates)
+    Vdot_max = coolant_flows[i_max]
 
-    # tabulate the results
-    data = [['t_opt', f'{t_opt}', 'min'],
-    ['Y_D/A', f'{yield_opt}', 'mol D per initial mol A'],
-    ['Conversion',f'{f_opt}', '%']]
-    results_df = pd.DataFrame(data, columns=['item','value','units'])
-
-    # display the results
-    print(' ')
-    print(results_df)
-    print(' ')
-
-    # save the results
-    results_df.to_csv('example_7_4_3_results.csv' , index=False)
+    # solve the reactor design equations using the optimum coolant flow
+    g_Vdot_ex = Vdot_max
+    t_2, nA_2, nZ_2, T_2, _ = stage2_bstr_model_variables(t_1[-1], nA_1[-1], nZ_1[-1]
+                                                , T_1[-1], Tex_1[-1])
     
-    # plot, show, and save the yield vs. the reaction time
-    plt.figure(1)
-    plt.plot(t, yield_DA)
+    # combine the profiles for the two stages
+    t = np.concatenate((t_1, t_2))
+    nA = np.concatenate((nA_1, nA_2))
+    nZ = np.concatenate((nZ_1, nZ_2))
+    T = np.concatenate((T_1, T_2))
+    
+    # calculate the conversion vs time at the optimum coolant flow rate
+    pct_conversion = 100*(nA_0 - nA)/nA_0
+    
+    # tabulate, show, and save the results
+    max_net_rate = nZ[-1]/(t[-1] + t_turn)
+    data = [["Optimum Coolant Flow", f"{Vdot_max:.0f}", "g min^-1^"],
+            ["Maximum Net Rate", f"{max_net_rate:.4f}", "mol min^-1^"]]
+    results_df = pd.DataFrame(data, columns=['item','value','units'])
+    print('')
+    print(results_df)
+    print('')
+    results_df.to_csv('example_7_4_3_results.csv', index=False)
+
+    # generate, show, and save the graphs
+    plt.figure(1) # net rate vs coolant flow
+    plt.plot(coolant_flows, net_rates)
+    plt.xlabel("Coolant Flow (g/min)")
+    plt.ylabel("Net Rate (mol/min)")
+    plt.savefig('example_7_4_3_net_rate_vs_coolant_flow.png')
+    plt.savefig('example_7_4_3_net_rate_vs_coolant_flow.pdf')
+    plt.savefig('../../../RE_Basics/solutions/ch7_ex3/example_7_4_3_net_rate_vs_coolant_flow.png')
+    plt.show(block=False)
+
+    plt.figure() # conversion profile
+    plt.plot(t,pct_conversion)
     plt.xlabel("Reaction Time (min)")
-    plt.ylabel("Yield (mol D per initial mol A)")
+    plt.ylabel("Conversion (%)")
     plt.xlim(left=0)
-    plt.savefig('example_7_4_3_yield_vs_t.pdf')
-    plt.savefig('example_7_4_3_yield_vs_t.png')
-    plt.savefig('../../../RE_Basics/solutions/ch7_ex3/example_7_4_3_yield_vs_t.png')
+    plt.ylim(bottom = 0)
+    plt.savefig('example_7_4_3_conversion_profile.png')
+    plt.savefig('example_7_4_3_conversion_profile.pdf')
+    plt.savefig('../../../RE_Basics/solutions/ch7_ex3/example_7_4_3_conversion_profile.png')
+    plt.show(block=False)
+
+    plt.figure() # temperature profile
+    plt.plot(t,T - 273.15)
+    plt.xlabel("Reaction Time (min)")
+    plt.ylabel("Temperature (°C)")
+    plt.xlim(left=0)
+    plt.savefig('example_7_4_3_temperature_profile.png')
+    plt.savefig('example_7_4_3_temperature_profile.pdf')
+    plt.savefig('../../../RE_Basics/solutions/ch7_ex3/example_7_4_3_temperature_profile.png')
     plt.show()
+
     return
 
+# execution command
 if __name__=="__main__":
     deliverables()
